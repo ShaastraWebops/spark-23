f3b2ffdd2c8ec68508a07b705668cf316ee6279b
arnav <goelarnav12@gmail.com>
2022-07-20 10:35:10 +0530

    Second Commit

--- src/Components/schedule.js
+++ src/Components/schedule.js
@@ -8,82 +8,147 @@ const Schedule = () => {
         </div>
         <div className="schedule">
             <div className="schedulehead schedrow">
+                <div className="headvalue1">City</div>
                 <div className="vlhead vl1"></div>
+                <div className="headvalue2">Venue Name</div>
                 <div className="vlhead vl2"></div>
+                <div className="headvalue3">Venue address</div>
                 <div className="vlhead vl3"></div>
+                <div className="headvalue4">Date</div>
                 <div className="vlhead vl4"></div>
+                <div className="headvalue5">Contact</div>
             </div>
             <div className="type1 schedrow">
+                <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>        
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>                
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
+            <div className="rowvalue1">Delhi</div>
                 <div className="vl vl1"></div>
+                <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
+                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="vl vl4"></div>
+                <div className="rowvalue4">20-07-2019</div>
+                <div className="vl vl4"></div>        
+                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
         </div>
     </section>
--- src/Components/structure.js
+++ src/Components/structure.js
@@ -7,26 +7,75 @@ const Structure = () => {
         <h1 className="Heading">STRUCTURE</h1>
       </div>
       <div className="info">
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        <p>SPARK is a three-tier mega event, with the first phase being the
+            SPARK Junior Quiz, a pan-India quiz competition to be conducted in
+            over 20 cities, far and wide across the nation. With 4000+
+            participants, the SPARK Junior Quiz is indeed one epic quizzing
+            Journey. The winners stand a chance to travel all the way to IIT
+            Madras free of cost to compete in the national finals and experience
+            what Shaastra has to offer.</p>
       </div>
       
       <div className="content1 content">
-        <div className="prelims contentinfo">
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, a</p>
+        <div className="prelims contentinfo"> 
+        <h1>Spark Prelims</h1>
+          <p>
+          <ul>
+              <li className="">
+                General Tech Quiz - science facts, logic, tech and how things
+                work
+              </li>
+              <li className="">
+                Written Round 40 questions - one word answer
+              </li>
+              <li className="">Teams of 2</li>
+              <li className="">Top 3 teams get prizes</li>
+              <li className="">
+                Top 5 teams from each city qualify for Nationals Screening round
+              </li>
+            </ul>
+          </p>
         </div>
         <div className="prelimsimage contentimage"></div>
       </div>
 
       <div className="content content2">
       <div className="screening contentinfo">
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when posed to using 'Content here, content here', making it look like readable English.</p>
+        <h1>Screening Round</h1>
+        <p>
+        <ul>
+              <li className="">
+                Questionnaire - 1 Page report on given topic (case study
+                competition)
+              </li>
+              <li className="">Online submission</li>
+              <li className="">
+                Participants will be guided by the Shaastra team
+              </li>
+              <li className="">
+                Top 25 go for the National finals - the Spark Junior Conference
+              </li>
+            </ul>
+        </p>
       </div>
       <div className="screeningimage contentimage"></div>
       </div>
 
       <div className="content content3">
       <div className="final contentinfo">
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when posed to using 'Content here, content here', making it look like readable English.</p>
+        <h1>Spark Finals</h1>
+        <p>
+        <ul>
+              <li className="">
+                Presentation - based on the report submitted for screening round
+              </li>
+              <li className="">Keynote and lectures</li>
+              <li className="">Workshops & Events</li>
+              <li className="">Sponsored trip to IIT Madras</li>
+              <li className="">Top 3 teams as National Winners</li>
+              <li className="">Date: TBA(During Shaastra)</li>
+            </ul>
+        </p>
       </div>
       <div className="finalimage contentimage"></div>
       </div>
