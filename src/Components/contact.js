8fd66df6ffa2f5b29a522bc15b38eb024690fdc9
arnav <goelarnav12@gmail.com>
2022-07-21 08:46:26 +0530

    Third Commit

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -12,23 +12,22 @@ const Contact = () => {
         </div>
         <div className="info">
             <h3>Contact Us at</h3>
-            <p>outreach@shaastra.org</p>
+            <p><a href="mailto: outreach@shaastra.org">outreach@shaastra.org</a></p>
       </div>
       <div className="footer">
         <div className="icons">
             {/* <div className="insta"> <i className="fa-brands fa-instagram-square"></i></div> */}
-            <div className="insta icon"><FontAwesomeIcon icon={faInstagram} size="3x" bounce/></div>
-            <div className="linkedin icon"></div><FontAwesomeIcon icon={faLinkedin} size="3x" beat/>
-            <div className="yt icon"></div><FontAwesomeIcon icon={faYoutube} size="3x" spin/>
+            <div className="insta icon"><a href="https://www.instagram.com/shaastra_iitm"><FontAwesomeIcon icon={faInstagram} size="3x" bounce/></a></div>
+            <div className="linkedin icon"><a href="https://in.linkedin.com/company/shaastra-iit-madras"><FontAwesomeIcon icon={faLinkedin} size="3x" beat/></a></div>
+            <div className="yt icon"><a href="https://www.youtube.com/channel/UCgY2ugmW-BV2nMRFu-0qPZA"><FontAwesomeIcon icon={faYoutube} size="3x" spin/></a>
+            </div>
         </div>
         <div className="footerdata">
-            <p> © DEVELOPED BY ARNAV , GOVIND | WEB OPERATIONS | SHAASTRA | IIT MADRAS . ALL RIGHTS RESERVED</p>
+            <p> © DEVELOPED BY ARNAV , GOVIND | WEB OPERATIONS | SHAASTRA | IIT MADRAS ALL RIGHTS RESERVED</p>
         </div>
       </div>
     </section>
   );
 };
-
-{/* <FontAwesomeIcon icon="fa-brands fa-instagram-square" /> */}
 
 export default Contact;
