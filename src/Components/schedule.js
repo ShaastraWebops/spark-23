5974cd955ab19af7aa816775f4ee17e838380139
goelarnav12 <95991454+goelarnav12@users.noreply.github.com>
2022-09-02 21:37:26 +0530

    Update schedule.js

--- src/Components/schedule.js
+++ src/Components/schedule.js
@@ -41,7 +41,7 @@ const Schedule = () => {
                 <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
             <div className="type1 schedrow">
-            <div className="rowvalue1">Delhi</div>
+            <div className="rowvalue1">Vijayawada</div>
                 <div className="vl vl1"></div>
                 <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
@@ -52,13 +52,13 @@ const Schedule = () => {
                 <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
             </div>
             <div className="type2 schedrow">
-            <div className="rowvalue1">Delhi</div>
+            <div className="rowvalue1">Nagpur</div>
                 <div className="vl vl1"></div>
                 <div className="rowvalue2">Kendriya Vidyalaya</div>
                 <div className="vl vl2"></div>
                 <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
                 <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
+                <div className="rowvalue4">18-09-2022</div>
                 <div className="vl vl4"></div>        
                 <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
             </div>
