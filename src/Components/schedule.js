4c68b2deba102f3cb027b42fb6503533cf95a9d2
krithikaa22 <kuruthiga22@gmail.com>
2022-09-03 02:36:11 +0530

    updated venue

--- src/Components/schedule.js
+++ src/Components/schedule.js
@@ -29,7 +29,7 @@ const Schedule = () => {
                 <div className="vl vl3"></div>
                 <div className="rowvalue4">05-09-2022</div>
                 <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>        
+                {/* <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>         */}
             </div>
             <div className="type2 schedrow">
             <div className="rowvalue1">Patna</div>
@@ -40,118 +40,31 @@ const Schedule = () => {
                 <div className="vl vl3"></div>
                 <div className="rowvalue4">04-09-2022</div>
                 <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
+                {/* <div className="rowvalue5">B.Sundar Balarka ,8333045402</div> */}
             </div>
             <div className="type1 schedrow">
             <div className="rowvalue1">Vijayawada</div>
                 <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
+                <div className="rowvalue2"></div>
                 <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
+                <div className="rowvalue3"></div>
                 <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
+                <div className="rowvalue4">11-09-2022</div>
                 <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
+                {/* <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                 */}
             </div>
             <div className="type2 schedrow">
             <div className="rowvalue1">Nagpur</div>
                 <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
+                <div className="rowvalue2"></div>
                 <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
+                <div className="rowvalue3"></div>
                 <div className="vl vl3"></div>
                 <div className="rowvalue4">18-09-2022</div>
                 <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
+                {/* <div className="rowvalue5">B.Sundar Balarka ,8333045402</div> */}
             </div>
-            <div className="type1 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
-            </div>
-            <div className="type2 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
-            </div>
-            <div className="type1 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
-            </div>
-            <div className="type2 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
-            </div>
-            <div className="type1 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
-            </div>
-            <div className="type2 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
-            </div>
-            <div className="type1 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>                
-            </div>
-            <div className="type2 schedrow">
-            <div className="rowvalue1">Delhi</div>
-                <div className="vl vl1"></div>
-                <div className="rowvalue2">Kendriya Vidyalaya</div>
-                <div className="vl vl2"></div>
-                <div className="rowvalue3">A.A.I, Rangpuri, Delhi, New Delhi, Delhi 110037</div>
-                <div className="vl vl3"></div>
-                <div className="rowvalue4">20-07-2019</div>
-                <div className="vl vl4"></div>        
-                <div className="rowvalue5">B.Sundar Balarka ,8333045402</div>
-            </div>
+           
         </div>
     </div>
   );
